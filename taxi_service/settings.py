5b0736e5cfc21b30ea9dcb7d65a30e312a651389
Mateusz <mateusz.rudnicki05@wp.pl>
2026-04-01 14:10:10 +0200

    Code

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -33,6 +33,9 @@ INTERNAL_IPS = [
     "127.0.0.1",
 ]
 
+CRISPY_TEMPLATE_PACK="bootstrap4"
+
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -44,6 +47,8 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "debug_toolbar",
     "taxi",
+    "crispy_bootstrap4",
+    "crispy_forms",
 ]
 
 MIDDLEWARE = [
